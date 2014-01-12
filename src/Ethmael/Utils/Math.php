3548a80d0c47d7923db28144cf81ab74e2f6d2a4
Ouiyam <william@myrtille.org>
2014-01-12 20:50:26 +0100

    error cases in randomN

--- src/Ethmael/Utils/Math.php
+++ src/Ethmael/Utils/Math.php
@@ -4,17 +4,37 @@ namespace Ethmael\Utils;
 
 class Math
 {
-    public static function randomN($nb_a_tirer, $val_min, $val_max)
+    /**
+     * Random function that return an array of values :
+     * - All values are different
+     * - All values are between a min and a max.
+     * @nbValues : number of values to return randomly
+     * @valMin : minimum value for values to return
+     * @valMax : maximum value for values to return
+     * return : array with all the random values
+     * Exception : RangeException if valMax - valMin < nbValues
+     * Exception : RangeException if valMax <= valMin
+     */
+    public static function randomN($nbValues, $valMin, $valMax)
     {
-        $tab_result = array();
-        while ($nb_a_tirer != 0) {
-            $nombre = mt_rand($val_min, $val_max);
-            if (!in_array($nombre, $tab_result)) {
-                $tab_result[] = $nombre;
-                $nb_a_tirer--;
-            }
+        if ($valMax <= $valMin) {
+            $message = sprintf('valMax <= valMin (%d <= %d)', $valMax, $valMin);
+            throw new \RangeException($message);
         }
 
+        if (($valMax - $valMin)<$nbValues) {
+            $message = sprintf('Not possible to get %d values between %d and %d', $nbValues,$valMin, $valMax);
+            throw new \RangeException($message);
+        }
+
+        $tab_result = array();
+        while ($nbValues != 0) {
+            $number = mt_rand($valMin, $valMax);
+            if (!in_array($number, $tab_result)) {
+                $tab_result[] = $number;
+                $nbValues--;
+            }
+        }
         return $tab_result;
     }
 }
